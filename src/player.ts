987642c9a9eb2db90436cdb35a11e25bff6497af
Dominic Fritschi <dominic.fritschi@webtiser.ch>
2022-12-16 18:59:17 +0100

    Init

---
+++ src/player.ts
@@ -0,0 +1,7 @@
+import { NS } from '@ns'
+
+export async function main(ns : NS) : Promise<void> {
+
+    ns.tprintf("Date [ %s ] - Karma [ %d ]", new Date().toLocaleString("de-CH"), ns.heart.break());
+
+}
