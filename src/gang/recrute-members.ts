d4b6dc2a9f5467fac81056f3f82ad207f4bb8994
Dominic Fritschi <dominic.fritschi@webtiser.ch>
2022-12-17 19:38:43 +0100

    Init

--- src/gang/recrute-members.ts
+++ src/gang/recrute-members.ts
@@ -1,14 +1,12 @@
 import { NS } from '@ns'
 
 export async function main(ns : NS) : Promise<void> {
-    const GANG_MEMBER_NAME_PREFIX = "Gugu";
-
     if(ns.gang.canRecruitMember()) {
         const gangMembers = ns.gang.getMemberNames().length;
-        const memberName = GANG_MEMBER_NAME_PREFIX + gangMembers;
-        const recured = ns.gang.recruitMember(memberName);
+        const memberName = (gangMembers + 1).toString();
+        const recrued = ns.gang.recruitMember(memberName);
 
-        if(recured) {
+        if(recrued) {
             ns.gang.setMemberTask(memberName, "Train Combat");
         } else {
             ns.tprintf("Can not recrute new member [ %s ]", memberName)
